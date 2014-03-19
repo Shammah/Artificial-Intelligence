bd35286c4feb5861b4cb890ebfd63627eee2bb19
Jaczz <jacco_snoeren@live.nl>
2014-03-19 21:35:50 +0100

    Implemented discreteSplit

--- assignment2/HandWrittenDigits/src/nl/tue/s2id90/classification/Main.java
+++ assignment2/HandWrittenDigits/src/nl/tue/s2id90/classification/Main.java
@@ -26,27 +26,64 @@ import nl.tue.s2id90.classification.labeledtree.DotUtil;
 public class Main {
 
     public static void main(String[] a) {
-        golf();
+        treeImages();
     }
-    
+
     public static void golf() {
         GolfData data = new GolfData();
         // Create dataset for confusion matrix.
         LabeledDataset2 dataSet = new LabeledDataset2();
         dataSet.putAll(data.getClassification());
-            
+
         DecisionTree43<Features, GolfData.PLAY> tree = new DecisionTree43<>(dataSet);
+        //DotUtil.showDotInFrame(tree.toDot(), "Golf");
         DotUtil.showDotInBrowser(tree.toDot());
     }
-    
+
     public static void ski() {
         SkiData data = new SkiData();
         // Create dataset for confusion matrix.
         LabeledDataset2 dataSet = new LabeledDataset2();
         dataSet.putAll(data.getClassification());
-            
+
         DecisionTree43<Features, SkiData.SKIING> tree = new DecisionTree43<>(dataSet);
-        DotUtil.showDotInBrowser(tree.toDot());
+        DotUtil.showDotInFrame(tree.toDot(), "Ski");
+    }
+
+    public static void treeImages() {
+        try {
+            // Load test and training data.
+            List<LabeledImage> trainingImages, testImages;
+            trainingImages = HandWrittenDigits.getTrainingData(600, true);
+            testImages = HandWrittenDigits.getTestData();
+
+            // Show test data.
+            //DigitsUtil.showImages("woei!", testImages.subList(0, 25), 5);
+            // Convert training data to a format that Classifier understands.
+            Map<ImageFeatures<Double>, Byte> trainingDataset, testDataset;
+            trainingDataset = new HashMap<>();
+            for (LabeledImage image : trainingImages) {
+                trainingDataset.put(new Doubles(image), image.getLabel());
+            }
+
+            testDataset = new HashMap<>();
+            for (LabeledImage image : testImages) {
+                testDataset.put(new Doubles(image), image.getLabel());
+            }
+
+            // Create dataset for confusion matrix.
+            LabeledDataset2 testData = new LabeledDataset2();
+            testData.putAll(testDataset);
+            LabeledDataset2 trainingData = new LabeledDataset2();
+            trainingData.putAll(trainingDataset);
+
+            // Runs the neirest neighbour algorithm and show the confusion matrix.
+            DecisionTree43<ImageFeatures<Double>,Byte> tree;
+            tree = new DecisionTree43<>(trainingData);
+            new ConfusionMatrixPanel(testData, tree.getConfusionMatrix(testDataset)).showIt();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public static void knn() {
--- assignment2/HandWrittenDigits/src/nl/tue/s2id90/classification/decisiontree/DecisionTree43.java
+++ assignment2/HandWrittenDigits/src/nl/tue/s2id90/classification/decisiontree/DecisionTree43.java
@@ -3,6 +3,7 @@ package nl.tue.s2id90.classification.decisiontree;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import nl.tue.s2id90.classification.data.Features;
@@ -19,54 +20,90 @@ public class DecisionTree43<F extends Features, L> extends DecisionTree<F, L> {
     public DecisionTree43(LabeledDataset2 dataset) {
         super(dataset);
     }
-    
+
     public DecisionTree43(LabeledDataset2 dataset, L clazz) {
         super(dataset, clazz);
     }
 
     @Override
     protected void build(LabeledDataset2<F, L> dataset) {
-        int maxAttr = -1;
-        double maxGain = -1;
-
-        // Find the attribute that gives the highest gain.
-        for (int i = 0; i < dataset.getNumberOfDimensions(); i++) {
+        System.out.println((dataset == null) + " " + dataset.isEmpty());
+        int splitIndex = -1; //index of attribute on which to split
+        double maxGain = -1; //maximum gain
+        //Find the attribute that gives the highest gain
+        for (int i = 0; i < dataset.getNumberOfDimensions(); i ++) {
             double gain = dataset.gain(i);
-
             if (gain > maxGain) {
                 maxGain = gain;
-                maxAttr = i;
+                splitIndex = i;
             }
         }
 
+        /** Base case */
+        System.out.println("maxGain: " + maxGain);
+        if (maxGain <= 0.000001) {
+            this.setLabel(dataset.getMostFrequentClass());
+            return;
+        }
+        /** Step case */
+        Map<Object, LabeledDataset2<F,L>> split;
+        if (dataset.isContinuousFeature(splitIndex)) {
+            //Continuous feature, We split at the median
+            List<Double> numbers = new ArrayList<>();
+            for (F feature : dataset.featureVectors()) {
+                numbers.add((Double) feature.get(splitIndex));
+            }
+            Collections.sort(numbers);
+            int median = (int) numbers.size() / 2;
+            this.setSplit(splitIndex, numbers.get(median));
+            split = dataset.continuousSplit(splitIndex, numbers.get(median));
+        } else {
+            //Concrete feature
+            this.setSplit(splitIndex, null);
+            split = dataset.discreteSplit(splitIndex);
+        }
+
+        for (Object value : split.keySet()) {
+            LabeledDataset2<F,L> subDataset = split.get(value);
+            if (!dataset.isEmpty()) {
+                               System.out.println(value);
+                DecisionTree43<F,L> subTree = new DecisionTree43<F,L>(subDataset);
+                this.addSubTree(value, subTree);
+            } else {
+                DecisionTree<F,L> subTree = new DecisionTree43<F,L>(subDataset,dataset.getMostFrequentClass());
+                this.addSubTree(value, subTree);
+                return;
+            }
+        }
+/*
         // We are at a leaf.
         if (maxGain <= 0.0001) {
             setLabel(dataset.getMostFrequentClass());
             return;
         }
-        System.out.println(maxGain);
 
         // Split the tree by the attribute giving the highest gain.
         Map<Object, LabeledDataset2<F, L>> split;
-
         if (dataset.isContinuousFeature(maxAttr)) {
             // Splitvalue = median.
             List<Integer> numbers = new ArrayList<>();
             for (F f : dataset.featureVectors()) {
                 numbers.add((int) f.get(maxAttr));
             }
-            
+
             Collections.sort(numbers);
+            System.out.println((int) (numbers.size() / 2) + " "  + numbers.get((int) (numbers.size() / 2)));
             split = dataset.continuousSplit(maxAttr, numbers.get((int) (numbers.size() / 2)));
         } else {
             split = dataset.discreteSplit(maxAttr);
+            this.setLabel((L) dataset.getFeatureName(maxAttr));
         }
 
         // Create subtrees from splitting.
         for (Object attribute : split.keySet()) {
             DecisionTree43<F, L> subTree;
             LabeledDataset2<F, L> subDataset = split.get(attribute);
-            
+
             if (split.keySet().size() == 1) {
                 subTree = new DecisionTree43<>(subDataset, subDataset.getLabels().iterator().next());
             } else {
@@ -74,6 +111,40 @@ public class DecisionTree43<F extends Features, L> extends DecisionTree<F, L> {
             }
 
             addSubTree(attribute, subTree);
+        }*/
+    }
+
+    @Override
+    public L classify(F v) {
+        DecisionTree<F, L> root = this;
+        if (root.isLeaf()) {
+            return root.getLabel();
+        } else {
+            int index = root.getSplitAttribute();
+            Object attributeValue = v.get(index);
+            root = root.getSubTree(attributeValue);
+            return root.classify(v);
         }
+    }
+
+    @Override
+    public Map<L, Map<L, Integer>> getConfusionMatrix(Map<F, L> testData) {
+        //initialize Confusion Matrix
+        Map<L, Map<L, Integer>> matrix = new HashMap<L, Map<L, Integer>>();
+        for (L label : testData.values()) {
+            matrix.put(label, new HashMap<L, Integer>());
+            for (L label2 : testData.values()) {
+                matrix.get(label).put(label2, 0);
+            }
+        }
+        // fill confusion matrix with values
+        for (F data : testData.keySet()) {
+            L correctLabel = testData.get(data);
+            L classifiedLabel = classify(data);
+            Integer currentFrequency = matrix.get(correctLabel).
+                    get(classifiedLabel);
+            matrix.get(correctLabel).put(classifiedLabel, currentFrequency + 1);
+        }
+        return matrix;
     }
 }
