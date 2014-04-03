13fec776cc9e36bbf66315b0fe262639f03ffcd8
Jaczz <jacco_snoeren@live.nl>
2014-04-03 08:26:36 +0200

    Comments of BayesianNetwork43.java

--- assignment3/BayesianNetwork43/src/bayesiannetwork43/BayesianNetwork43.java
+++ assignment3/BayesianNetwork43/src/bayesiannetwork43/BayesianNetwork43.java
@@ -3,10 +3,8 @@ package bayesiannetwork43;
 import bayesiannetwork43.ProbabilityTable.Row;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.StringReader;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -21,18 +19,31 @@ import java.util.logging.Logger;
 import nl.tue.win.util.Pair;
 
 /**
- *
+ * This class represents our Bayesian network.
+ * 
  * @author Group 43
  */
 public class BayesianNetwork43 {
 
+    /** The tables in the bayesian network. */
     public List<ProbabilityTable> _tables;
-    private String queryVar;
-    private Map<String, String> givenValues = new HashMap<>();
 
-    private final static String FILE = "spiegelhalter.txt";//"spiegelhalter.txt";
+    /** The query variable. */
+    private String queryVar;
+
+    /** {@code Map} containing the fixed variables and their values. */
+    private final Map<String, String> givenValues = new HashMap<>();
+
+    /** The file which we use to create this bayesian network.
+     * "spiegelhalter.txt" gives the BirthAsphyxia network, while
+     * "alarm.txt" gives the burglary network.
+     */
+    private final static String FILE = "spiegelhalter.txt";
 
     /**
+     * Main method. Initializes the Bayesian network, reads the query, computes
+     * the result and writes the output to the screen.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -52,9 +63,19 @@ public class BayesianNetwork43 {
         bn.writeOutput(output);
     }
 
+    /**
+     * Reads the query from {@code System.in}. For testing purposes, an input
+     * via {@code String} is also possible. If this {@code String} is {@code null},
+     * input from {@code System.in} is used.
+     *
+     * @param input possible input via {@code String}
+     * @modifies {@code this.queryVar, this.givenValues}
+     * @post the query has been read properly
+     */
     public void readQuery(String input) {
         Scanner scanner;
         if (input != null) {
+            //test case
             scanner = new Scanner(input);
         } else {
             scanner = new Scanner(System.in);
@@ -62,11 +83,11 @@ public class BayesianNetwork43 {
         scanner.next(); //ignore P(
         queryVar = scanner.next(); //store query variable
         scanner.next(); //ignore |
-        boolean stop = false;
-        while (!stop) {
-            String variable = scanner.next();
+        boolean stop = false; //determines whether we need to stop
+        while (! stop) {
+            String variable = scanner.next(); //the next variable and value pair
             if (! (variable.equals(")"))) { //end of query
-                variable = variable.replaceFirst("=", " ");
+                variable = variable.replaceFirst("=", " "); //replaceFirst since there are data containing e.g. >= 5.0
                 String[] parts = variable.split("\\s+"); // split on space
                 variable = parts[0];
                 String value = parts[1].replaceAll(",", "");
@@ -77,22 +98,26 @@ public class BayesianNetwork43 {
         }
     }
 
+    /**
+     * Executes the variable elimination algorithm.
+     *
+     * @return A {@code ProbabilityTable} containing n rows if {@code queryVar}
+     * is n-valued, where the probability is the probability asked for in the query.
+     */
     public ProbabilityTable eliminate() {
-        ProbabilityTable result;
+        ProbabilityTable result; //the resulting {@code ProbabilityTable}
         Set<String>  relevantVariables      = findRelevantVariables();
         List<String> topologicalVariables   = topologicalSort();
 
+        // we loop over the variables in (reversed) topological sorted order.
         Iterator<String> iter               = topologicalVariables.iterator();
-        String firstVar = iter.next();
-        while (true) {
-            if (relevantVariables.contains(firstVar)) {
-                result                      = getTable(firstVar);
-                break;
-            } else {
-                firstVar = iter.next();
-            }
+        String firstVar = iter.next(); //the first relevant factor in the sorted order
+        while (! relevantVariables.contains(firstVar)) {
+            firstVar = iter.next();
         }
+        result                              = getTable(firstVar);
 
+        // we apply a filter according to the query if the factor is a fixed variable
         List<Pair<String, String[]>> filter = new ArrayList<>();
         String filterValue                  = givenValues.get(result.getName());
         if (filterValue != null) {
@@ -100,54 +125,69 @@ public class BayesianNetwork43 {
             result                              = result.filter(filter);
         }
 
+        // loop over all relevant variables
         while (iter.hasNext()) {
             String variable = iter.next();
-            if (relevantVariables.contains(variable)) {
+            if (relevantVariables.contains(variable)) { // we skip nonrelevant variables
+                ProbabilityTable varTable = getTable(variable); //next factor
                 // Are we fixed or free?
-                ProbabilityTable varTable = getTable(variable);
                 if (givenValues.containsKey(variable)) {
+                    //fixed variable. we filter according to the query and multiply with the result
                     filter = new ArrayList<>();
                     filter.add(new Pair<>(variable, new String[]{givenValues.get(variable)}));
                     varTable = varTable.filter(filter);
                     result = result.multiply(varTable);
                 } else if (!variable.equals(queryVar)) {
+                    // free variable. We multiply and marginalize. (eliminate the variable)
                     result = result.multiply(varTable).marginalize(variable);
                 } else {
+                    // query variable. multiply the full table withour eliminating.
                     result = result.multiply(varTable);
                 }
             }
         }
-
+        //normalize the result such that the probabilities sum up to one.
         return result.normalize();
     }
 
+    /**
+     * Returns a reversed topological sort over the complete bayesian network.
+     *
+     * @return a {@code List} containing all variables such that a parent of a
+     * vertex is always found further in the {@code List}
+     */
     public List<String> topologicalSort() {
         List<String> result = new ArrayList<>();
         Set<String> variables = new HashSet<>(getVariables());
         Set<String> leaves;
 
         while (! variables.isEmpty()) {
-            leaves = findLeaves(variables);
-            Set<String> toRemove = new HashSet<>();
+            leaves = findLeaves(variables); //find leaves only on these variables
+            Set<String> toRemove = new HashSet<>(); //leaves to be removed
             for (String node : variables) {
-                if (leaves.contains(node)) {
+                if (leaves.contains(node)) { // we have a leaf
                     result.add(node);
                     toRemove.add(node);
                 }
             }
-            variables.removeAll(toRemove);
+            variables.removeAll(toRemove); //remove all leaves from the network
             toRemove.clear();
         }
         return result;
     }
 
     /**
-     * Finds all leaves in the Bayesian network.
-     * @return a list of leaves.
+     * Finds all leaves in the Bayesian network containing only the vertexes
+     * corresponding to {@code variables}.
+     *
+     * @param variables the variables that we want to consider in the network
+     * @pre {@code variables != null}
+     * @return a {@code Set} containing the leaves in the network, excluding any
+     * vertexes not in {@code variables}
      */
     public Set<String> findLeaves(Set<String> variables) {
-        variables = new HashSet(variables);
-        Set<String> parents = new HashSet();
+        variables = new HashSet(variables); // needed so we do not modify the actual variables
+        Set<String> parents = new HashSet(); //set containing all variables that are parents
 
         for (String variable : variables) {
            for (ProbabilityTable table : _tables) {
@@ -164,17 +204,22 @@ public class BayesianNetwork43 {
     }
 
     /**
-     * Returns the list of all tables that were imported.
-     * @return the list of all tables.
+     * Returns a {@code List} containing all tables that were imported.
+     *
+     * @return a {@code List} containing all tables that were imported
      */
     public List<ProbabilityTable> getTables() {
         return _tables;
     }
 
     /**
-     * Returns a table by looking for its table name.
-     * @param header The name of the table.
-     * @return The table if present, null otherwise.
+     * Returns the {@code ProbabilityTable} which corresponds to variable
+     * {@code header}. Uses simple linear search.
+     *
+     * @param header The name of the {@code ProbabilityTable}
+     * @pre {@code header != null}
+     * @return The {@code ProbabilityTable} corresponding to {@code header} if
+     * {@code header} is a variable in the Bayesian network, {@code null} otherwise.
      */
     public ProbabilityTable getTable(String header) {
         ProbabilityTable result = null;
@@ -188,8 +233,9 @@ public class BayesianNetwork43 {
     }
 
     /**
-     * Returns a list of all variables in the network.
-     * @return a list of all variables in the network.
+     * Returns a {@code List} containing all variables in this Bayesian network.
+     * In other words, all headers in the data set.
+     * @return a {@code List} containing all variables in this Bayesian network.
      */
     public List<String> getVariables() {
         List<String> variables = new ArrayList<>();
@@ -201,23 +247,37 @@ public class BayesianNetwork43 {
         return variables;
     }
 
+    /**
+     * Finds the relevant variables in this Bayesian network according to the
+     * given query. A variable is irrelevant if it does not influence the result
+     * of the query. Since only parents and ancestors influence a particular
+     * variable, we find the variables that are ancestors of the variables given
+     * in the query.
+     *
+     * @return a {@code Set} containing the relevant variables in this Bayesian
+     * network according to the given query.
+     */
     public Set<String> findRelevantVariables() {
         Set<String> relevantVars = new HashSet<>();
-        relevantVars.add(queryVar);
+        relevantVars.add(queryVar); //the query variable is relevant
 
         // Add all variables from the given set.
         for (String variable : givenValues.keySet()) {
-            relevantVars.add(variable);
+            relevantVars.add(variable); // the given variables are relevant
         }
 
+        //{@code List} representing the vertexes on the current level at which we are looking
         List<String> currentNodes = new ArrayList<>(relevantVars);
+        //{@code List} containing all vertexes that we will be looking at during the next iteration
         List<String> futureNodes = new ArrayList<>();
         while (! currentNodes.isEmpty()) {
             for (String currentNode : currentNodes) {
-                List<String> parents = new ArrayList(getTable(currentNode).getParents());
+                List<String> parents = new ArrayList(getTable(currentNode).getParents()); //find the parents
                 while (! parents.isEmpty()) {
                     List<String> toRemove= new ArrayList<>();
                     for (String parent : parents) {
+                        //the parent is relevant, we will look at it in the next round
+                        //and remove it since we added it to the relevant variables
                         relevantVars.add(parent);
                         futureNodes.add(parent);
                         toRemove.add(parent);
@@ -228,6 +288,7 @@ public class BayesianNetwork43 {
             }
             currentNodes.clear();
             List<String> toRemove= new ArrayList<>();
+            // go one level higher
             for (String node : futureNodes) {
                 currentNodes.add(node);
                 toRemove.add(node);
@@ -237,17 +298,29 @@ public class BayesianNetwork43 {
         return relevantVars;
     }
 
+    /**
+     * Writes the output of the query to the screen.
+     *
+     * @param probs {@code ProbabilityTable} containing n rows for an n-valued
+     * query variable, with fixed values for the given variables and one of the
+     * n values for the query variable
+     * @pre {@code probs != null}
+     * @post the result of the query is written to the screen
+     */
     public void writeOutput(ProbabilityTable probs) {
-        String output = "(";
+        String output = "("; //start with an opening parenthesis
+        //make sure the decimal seperator is a dot, not a comma.
         DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.ENGLISH);
         DecimalFormat format = new DecimalFormat("#.####", symbols);
 
         int columnIndex = probs.getColumnIndex(queryVar);
         for (Row row : probs.getRows()) {
+            //add the probability to the result, together with the label
             output += row.first.get(columnIndex) + " = " + format.format(row.second.getValue()) + ", ";
         }
+        //remove the last comma and space
         output = output.substring(0, output.length() - 2);
-        output += ")";
+        output += ")"; //add the closing parenthesis
         System.out.println(output);
     }
 }
